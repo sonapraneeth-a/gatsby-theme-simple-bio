430b5c89526f4655bcbced39d18a6dbf8596e3a1
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-07-30 21:28:44 +0530

    fix: removed options context from gatsby browser and ssr

--- gatsby-theme-simple-bio/gatsby-ssr.js
+++ gatsby-theme-simple-bio/gatsby-ssr.js
@@ -1,14 +1,8 @@
 import React from "react";
-import merge from "lodash.merge";
-
-import {defaultOptions, OptionsContext} from "./src/themes/options";
 
 // eslint-disable-next-line react/prop-types
 export const wrapRootElement = ({element}, themeOptions) => {
-  const computedOptions = merge({}, defaultOptions, themeOptions);
   return (
-    <OptionsContext.Provider value={computedOptions}>
-      {element}
-    </OptionsContext.Provider>
+    <>{element}</>
   );
 };
